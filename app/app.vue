755d68c5d199bbb908f22e6adfe261e9e7b735e4
Alexis Reina <alexis.reina@frontiersin.org>
2025-10-01 18:02:45 +0200

    chore: project config

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,14 @@
+<script setup lang="ts">
+import { Button } from 'primevue';
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="font-sans">
+    <Button label="Click" rounded />
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   </div>
 </template>
+
+
